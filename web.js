4c7c4f220072ec5015c19030959b4a028239827f
neonaleon <neon.aleon@hotmail.com>
2013-02-28 15:27:36 +0800

    web.js get readd

--- web.js
+++ web.js
@@ -22,3 +22,10 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
+function handle_request(req, res) {
+	res.redirect('/index.htm');
+}
+
+app.get('/', handle_request);
+app.post('/', handle_request);
