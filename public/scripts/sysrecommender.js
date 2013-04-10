f43082062ebebb92350345798ce7155395e8cb16
neonaleon <neon.aleon@hotmail.com>
2013-04-10 14:43:43 +0800

    debugging

--- public/scripts/sysrecommender.js
+++ public/scripts/sysrecommender.js
@@ -119,6 +119,7 @@ sysrecommender.do_recommendation = function () {
 			//this.recommendations[ i * this.topN_songs + j ] = this.friend_playlists[friend_id][Math.floor(Math.random() * this.friend_playlists[friend_id].length)];
 			var similar = this.top_similar_songs(this.friend_song_vectors[friend_id]);
 			this.recommendations[i*this.topN_songs+j] = similar[j][1];
+			console.log(similar);
 		}
 	}
 	this.end_recommendation();
@@ -130,6 +131,7 @@ sysrecommender.top_similar_songs = function (song_vectors) {
 		similar.push( [ this.cosine_similarity( song_vectors[k] ), k ] );
 	}
 	similar.sort( function(a, b){ return b[0] - a[0]; } );
+	console.log(similar);
 	return similar;
 }
 
@@ -263,6 +265,7 @@ sysrecommender.get_friend_playlist = function (id) {
 		},
 		success : function (response) {
 			sysrecommender.retrieved += 1;
+			console.log(response);
 			sysrecommender.friend_playlists[id] = response.playlist;
 			sysrecommender.friend_song_vectors[id] = response.vectors;
 			if (sysrecommender.retrieved == sysrecommender.toRetrieve)
